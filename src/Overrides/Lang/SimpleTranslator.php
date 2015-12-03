c98a198d222f30c2d1e497f920fc925c6f8c86fa
Tomas Joelsson <tomas@juwai.com>
2015-12-03 16:13:47 +0800

    JUW-1453 Add explanation for SimpleTranslation class

--- src/Overrides/Lang/SimpleTranslator.php
+++ src/Overrides/Lang/SimpleTranslator.php
@@ -4,6 +4,11 @@ namespace Juwai\LaravelBase\Overrides\Lang;
 use Illuminate\Translation\Translator;
 use Illuminate\Support\Str;
 
+/**
+ * This class makes translation strings shorter by allowing them to be passed
+ * without catalogue prefix. If a prefix is not supplied it adds a default one
+ * that can be configured in config/translation.php.
+ */
 class SimpleTranslator extends Translator
 {
     public function get($key, array $replace = array(), $locale = null)
